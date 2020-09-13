5af0ef5172847c700eaac3a35532c5c57f0980d9
Sandip <sandip.dhang@yahoo.com>
2020-09-13 21:56:00 +0530

    Hello World

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const Home = () => {
+  return (
+    <div>
+      <h1>SD Creations</h1>
+      <h2>If you like the video hit like button and subscribe us</h2>
+    </div>
+  );
+};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<Home />, document.getElementById("root"));
